1c271e54df0bcd96b5aca19f11d9fb798058b13e
asvarga <asvarga@gmail.com>
2024-04-03 11:14:07 -0300

    chore: add estados in server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
 import 'tsconfig-paths/register';
 import { server } from '@infra/server';
+import { Estados } from '@enums/estados';
 
 const myPort = process.env.PORT || 3333;
 
 server.listen(myPort, () => {
-    console.log('Server is running');
+    console.log(`Server is running in ${Estados.RJ}`);
 });
