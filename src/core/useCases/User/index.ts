597bbeda1f4c83ca6aa3b1c0b617fa073461fa77
asvarga <asvarga@gmail.com>
2024-04-04 11:22:06 -0300

    chore: add userRoutes and setup useCases

---
+++ src/core/useCases/User/index.ts
@@ -0,0 +1,6 @@
+import * as createUserZod from './createUser/CreateUserController';
+
+// export const UserController = {
+export const User = {
+    ...createUserZod,
+};
---
+++ src/routes/user/index.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express';
+import { Providers } from '@provider/index';
+import { User } from '@core/useCases';
+
+const UserRoutes = Router();
+
+UserRoutes.post('/userzod', User.CreateUserZodController);
+
+UserRoutes.get(
+    '/userauthenticated/:id',
+    Providers.Token.checkToken,
+    (request: Request, response: Response) => {
+        return response.json([
+            { id: 1, name: 'NodeJs' },
+            { id: 2, name: 'ReactJs' },
+            { id: 3, name: 'React Native' },
+            { id: 4, name: 'Flutter' },
+            { id: 5, name: 'Elixir' },
+        ]);
+    },
+);
+
+export default UserRoutes;
