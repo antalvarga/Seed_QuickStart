cd7e47d9aaab1228bf64608d4c557258c2728cba
asvarga <asvarga@gmail.com>
2024-04-12 09:46:39 -0300

    fix! : tunning

--- src/infra/prisma/client.ts
+++ src/infra/prisma/client.ts
@@ -3,3 +3,21 @@ import { PrismaClient } from '@prisma/client';
 const client = new PrismaClient();
 
 export { client };
+
+/*
+// Importa o PrismaClient
+import { PrismaClient } from '@prisma/client';
+
+// Declara o cliente fora da função (opcional)
+let client: PrismaClient;
+
+// Exporta a função de inicialização
+export const initializePrismaClient = async () => {
+    // Cria uma nova instância do PrismaClient
+    client = new PrismaClient();
+    console.log('PrismaClient initialized:', client);
+};
+
+// Exporta o cliente (opcional, necessário se usar a Solução 2)
+export { client };
+*/
