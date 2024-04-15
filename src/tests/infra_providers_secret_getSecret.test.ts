347eea2e0d3426552561661d811a17bfc8254ae1
asvarga <asvarga@gmail.com>
2024-04-15 17:13:10 -0300

    fix! : tunning getSecret

--- src/tests/infra_providers_secret_getSecret.test.ts
+++ src/tests/infra_providers_secret_getSecret.test.ts
@@ -3,6 +3,7 @@ import { MockUser } from './_mocks/user.mock';
 const getSecret = (userId: string) => {
     let user;
 
+    // Simula uma pesquisa
     if (userId == MockUser.id) {
         user = MockUser;
     }
@@ -17,21 +18,15 @@ const getSecret = (userId: string) => {
 };
 
 test('deve retornar o segredo do usuário quando encontrado', async () => {
-    // Simular a busca do usuário pelo ID '1' (mock do repositório)
     const usuarioEncontrado = MockUser;
 
-    // Chamar a função real com o ID do usuário
-    // const secret = await getSecret("1");
     const secret = getSecret('1');
 
-    // Verificar se o segredo retornado é igual ao segredo mockado
     expect(secret).toBe(usuarioEncontrado.secret);
 });
 
 test('deve retornar nulo se o usuário não for encontrado', async () => {
-    // Chamar a função real com o ID do usuário inexistente
     const secret = getSecret('ID_INEXISTENTE');
 
-    // Verificar se o segredo retornado é nulo
     expect(secret).toBeNull();
 });
